9067ce82c0508acbab112587e9a43bee1d46c126
Chris Hunt <huntca@gmail.com>
2011-08-15 14:17:15 -0700

    Implemented scramble for the Rubik's Cube

--- lib/puzzled/rubiks_cube.rb
+++ lib/puzzled/rubiks_cube.rb
@@ -1,8 +1,46 @@
 module Puzzled
   class RubiksCube
+    GROUPS = [["F","B"],["L","R"],["D","U"]]
+    DIRS   = ["","'","2"]
+
+    # Retun Rubik's Cube scramble of size = size
     def scramble(size=25)
       scramble = []
-      size.times { scramble << "F'" }
+      prev_group = nil
+      prev_prev_group = nil
+      size.times do
+        # Find a move group for the next move
+        # We cannot have 3 moves in a row that are in the same group
+        group = rand(GROUPS.size)
+        if prev_group == nil
+          prev_group = group
+        elsif prev_prev_group == nil
+          prev_prev_group = prev_group
+          prev_group = group
+        else
+          # While we have 3 moves in the same group, pick a new group
+          while group == prev_group && group == prev_prev_group
+            group = rand(GROUPS.size)
+          end
+          prev_prev_group = prev_group
+          prev_group = group
+        end
+
+        # Find the next move from move group
+        # We cannot have two turns in a row on the same face
+        move = GROUPS[group][rand(2)]
+        if scramble.size > 0
+          while move == scramble.last[0] do
+            move = GROUPS[group][rand(2)]
+          end
+        end
+
+        # Find a direction for this move
+        dir = DIRS[rand(DIRS.size)]
+
+        # Add move to scramble
+        scramble << move + dir
+      end
       return scramble
     end
   end
--- spec/rubiks_cube_spec.rb
+++ spec/rubiks_cube_spec.rb
@@ -25,5 +25,38 @@ describe Puzzled::RubiksCube do
         dirs.include?(move[1]).should == true if move.size > 1
       end
     end
+
+    it "does not rotate the same face 2 times in a row" do
+      prev = nil
+      @rubiks_cube.scramble(500).each do |move|
+        if prev != nil
+          move[0].should_not == prev
+        end
+        prev = move[0]
+      end
+    end
+
+    it "does not rotate parallel faces 3 times in a row" do
+      group = { "F" => 1, "B" => 1,
+                "L" => 2, "R" => 2,
+                "D" => 3, "U" => 3 }
+      prev = nil
+      prev_prev = nil
+      @rubiks_cube.scramble(500).each do |move|
+        if prev == nil
+          prev = move
+        elsif prev_prev == nil
+          prev_prev = prev
+          prev = move
+        else
+          if group[prev[0]] == group[move[0]]
+            group[prev_prev[0]].should_not == group[move[0]]
+          end
+          prev_prev = prev
+          prev = move
+        end
+      end
+    end
+
   end
 end
